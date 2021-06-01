5378b64d63aa4f72b7572735ab0fbf5b2b763fc2
Alexia Wilson <hello@lyxi.ca>
2021-06-01 08:48:46 -0700

    Initial Commit

---
+++ ListRecursiveFiletypes/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ListRecursiveFiletypes
+//
+//  Created by Alexia Wilson on 2021-06-01.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
