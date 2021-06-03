d28075140c1147eaeaa4202d5881093304916675
Alexia Wilson <hello@lyxi.ca>
2021-06-03 15:21:04 -0700

    Add .gitignore

--- ListRecursiveFiletypes/main.m
+++ ListRecursiveFiletypes/main.m
@@ -11,11 +11,12 @@
 #define var __auto_type
 
 NSFileManager *fm;
+typedef NSMutableDictionary<NSURL *, NSDate *> UrlAndDate;
 
-CustomMutDict* _get_file_types(NSURL *path, int depth, int max_depth)
+
+UrlAndDate* _get_file_types(NSURL *path, int depth, int max_depth)
 {
-    var dict = [CustomMutDict new];
-
+    UrlAndDate *dict = [NSMutableDictionary new];
     if (depth > max_depth)
     {
         return dict;
@@ -27,41 +28,49 @@ CustomMutDict* _get_file_types(NSURL *path, int depth, int max_depth)
         if ([url hasDirectoryPath])
         {
             var result = _get_file_types(url, depth + 1, max_depth);
-            [dict addCustomDict:result];
+            [dict addEntriesFromDictionary:result];
         }
         else
         {
-            var extension = url.pathExtension;
-            if ([@"" isEqualTo:extension])
-            {
-                continue;
-            }
-            [dict addNumber:@1 forKey:extension];
+            var attributes = [fm attributesOfItemAtPath:url.path error:nil];
+            dict[url] = [attributes fileModificationDate];
         }
     }
     
     return dict;
 }
 
-NSMutableDictionary<NSString *, NSNumber *>* get_file_types(NSURL *path, int depth)
+UrlAndDate* get_file_types(NSURL *path, int depth)
 {
     var dict = _get_file_types(path, 1, depth);
-    return [dict getDict];
+    return dict;
 }
 
 int main(int argc, const char * argv[])
 {
     fm = [NSFileManager defaultManager];
-    var current_folder = NSFileManager.defaultManager.currentDirectoryPath;
-    var results = get_file_types([[NSURL alloc] initFileURLWithPath:@"/Users"], 5);
+    var current_folder = argc == 1 ? NSFileManager.defaultManager.currentDirectoryPath : [NSString stringWithUTF8String:argv[1]];
+    var results = get_file_types([[NSURL alloc] initFileURLWithPath:current_folder], 5);
     var sorted_keys = [results keysSortedByValueUsingComparator:^(id obj1, id obj2) {
         return [obj2 compare:obj1];
     }];
     var output = [NSMutableString new];
+    var calendar = [NSCalendar currentCalendar];
+    var component_units = NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitWeekOfMonth | NSCalendarUnitDay | NSCalendarUnitHour | NSCalendarUnitMinute;
+    var formatter = [NSDateComponentsFormatter new];
+    formatter.collapsesLargestUnit = YES;
+    formatter.allowsFractionalUnits = YES;
+
+    formatter.unitsStyle = NSDateComponentsFormatterUnitsStyleBrief;
     
-    for (NSString *str in sorted_keys) {
-        [output appendFormat:@"%d\t => .%s\n", results[str].intValue, str.UTF8String];
+    for (NSURL *key in sorted_keys) {
+        var path = [key.path stringByReplacingOccurrencesOfString:current_folder withString:@"."];
+        var date = results[key];
+        var adjusted_date = [calendar components:component_units fromDate:date toDate:[NSDate now] options:0];
+        
+        [output appendFormat:@"%s ago\t%s\n", [formatter stringFromDateComponents:adjusted_date].UTF8String, path.UTF8String];
     }
+
     
     printf("%s\n", output.UTF8String);
     return 0;
